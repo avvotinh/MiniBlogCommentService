c476df2215e69c85ee959dd85c02e4658a6da1e5
Le Van Hop <hop.le@axonactive.com>
2021-07-07 18:27:31 +0700

    updated

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
   const comments = commentsByPostId[postId] || [];
-  const comment = { commentId, content };
+  const comment = { id: commentId, content, status: "pending" };
 
   comments.push(comment);
   commentsByPostId[postId] = comments;
@@ -32,14 +32,35 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId,
+      status: "pending",
     },
   });
 
   res.status(201).send(comment);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Received Event: ", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => comment.id === id);
+
+    comment.status = status;
+
+    await axios.post("http://localhost:4002/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        content,
+        status,
+        postId,
+      },
+    });
+  }
 
   res.send({});
 });
