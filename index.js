ffbbb416819f21a84d79446586af3e8255d7d113
Le Van Hop <hop.le@axonactive.com>
2021-07-07 16:54:35 +0700

    Update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -15,7 +16,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(comments);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const postId = req.params.id;
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
@@ -25,7 +26,22 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push(comment);
   commentsByPostId[postId] = comments;
 
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId,
+    },
+  });
+
   res.status(201).send(comment);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event: ", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => {
